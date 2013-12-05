aa8b85e7c4f81e0609573ab96922a79e540194aa
Mahmoud Ismail <maism@kth.se>
2013-12-05 12:40:08 +0100

    excess replica added

---
+++ src/main/java/se/sics/hop/metadata/persistence/dal/ExcessReplicaDataAccess.java
@@ -0,0 +1,28 @@
+package se.sics.hop.metadata.persistence.dal;
+
+import java.util.Collection;
+import java.util.List;
+import se.sics.hop.metadata.persistence.entity.HopExcessReplica;
+import se.sics.hop.metadata.persistence.exceptions.StorageException;
+
+/**
+ *
+ * @author kamal hakimzadeh<kamal@sics.se>
+ */
+public abstract class ExcessReplicaDataAccess extends EntityDataAccess {
+
+  public static final String TABLE_NAME = "excess_replicas";
+  public static final String BLOCK_ID = "block_id";
+  public static final String STORAGE_ID = "storage_id";
+
+  public abstract int countAll() throws StorageException;
+
+  public abstract List<HopExcessReplica> findExcessReplicaByStorageId(String sId) throws StorageException;
+  public abstract List<HopExcessReplica> findExcessReplicaByBlockId(long bId) throws StorageException;
+
+  public abstract HopExcessReplica findByPkey(Object[] params) throws StorageException;
+  
+  public abstract void prepare(Collection<HopExcessReplica> removed, Collection<HopExcessReplica> newed, Collection<HopExcessReplica> modified) throws StorageException;
+  
+  public abstract void removeAll() throws StorageException;
+}
