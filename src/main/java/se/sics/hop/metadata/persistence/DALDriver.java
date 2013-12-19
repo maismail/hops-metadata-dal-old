641cb201bfa8e54867a15fc2aac5e9325ab4678d
Mahmoud Ismail <maism@kth.se>
2013-12-19 13:59:41 +0100

    use class.forname given the jar is already in the classpath

--- src/main/java/se/sics/hop/metadata/persistence/DALDriver.java
+++ src/main/java/se/sics/hop/metadata/persistence/DALDriver.java
@@ -1,9 +1,5 @@
 package se.sics.hop.metadata.persistence;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import se.sics.hop.metadata.persistence.exceptions.StorageInitializtionException;
 
 /**
@@ -12,21 +8,14 @@ import se.sics.hop.metadata.persistence.exceptions.StorageInitializtionException
  */
 public class DALDriver {
 
-  //FIXME: remove the jar file path
-  public static DALStorageFactory load(String driverJarPath, String storageFactoryClassName) throws StorageInitializtionException{
+  public static DALStorageFactory load(String storageFactoryClassName) throws StorageInitializtionException {
     try {
-          URL driverPath = new File(driverJarPath).toURI().toURL();
-          ClassLoader cl = new URLClassLoader(new URL[]{driverPath});
-          Class sfc = cl.loadClass(storageFactoryClassName);
-          return (DALStorageFactory) sfc.newInstance();
-//          return (DALStorageFactory) Class.forName(storageFactoryClassName).newInstance();
+      return (DALStorageFactory) Class.forName(storageFactoryClassName).newInstance();
     } catch (ClassNotFoundException ex) {
       throw new StorageInitializtionException(ex);
     } catch (InstantiationException ex) {
       throw new StorageInitializtionException(ex);
     } catch (IllegalAccessException ex) {
-      throw new StorageInitializtionException(ex);
-    } catch (MalformedURLException ex) {
       throw new StorageInitializtionException(ex);
     }
   }
