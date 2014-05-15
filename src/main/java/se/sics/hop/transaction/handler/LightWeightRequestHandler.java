117b7f286c12ba37bc7db0e9ed8b28e96d375635
Theofilos <tkak@sics.se>
2014-05-15 19:01:48 +0200

    Improved rollback transaction

--- src/main/java/se/sics/hop/transaction/handler/LightWeightRequestHandler.java
+++ src/main/java/se/sics/hop/transaction/handler/LightWeightRequestHandler.java
@@ -10,58 +10,67 @@ import se.sics.hop.exception.PersistanceException;
  */
 public abstract class LightWeightRequestHandler extends RequestHandler {
 
-  public LightWeightRequestHandler(OperationType opType) {
-    super(opType);
-  }
-
-  @Override
-  protected Object run(Object info) throws IOException {
-    boolean retry = true;
-    int tryCount = 0;
-    IOException exception = null;
-
-    try {
-      while (retry && tryCount < RETRY_COUNT) {
-        retry = true;
-        tryCount++;
-        exception = null;
-        try {
-          NDC.push(opType.toString()); // Defines a context for every operation to track them in the logs easily.
-
-          //In a tx if the lock level is set to write, does 
-          //it mean that all the operations after seting the lock will use write lcok? 
-          //Light weight tx handler is sometimes called inside a bigger tx. 
-          //If in the outer tx lock level was set to some thing other than read-commited 
-          //then we will end up taking un necessary locks.
-          //To make sure that we done have this problem I explicitly set the locks to read-commited. 
-          
-         
-          //connector.beginTransaction();
-          //connector.readCommitted();
-          Object toReturn = performTask();
-          //connector.commit();
-          return toReturn;
-        } catch (PersistanceException ex) {
-          log.error("Could not perfortm task-"+ex.getMessage());
-          
-          retry = true;
-        } catch (IOException ex) {
-          exception = ex;
-        } catch (Exception ex) {
-          
-        }finally {
-            if(connector.isTransactionActive()){
-            connector.rollback();
-        }
-          NDC.pop();
-          
-        }
-      }
-    } finally {
-        if(connector.isTransactionActive()){
-            connector.rollback();
-        }
+    public LightWeightRequestHandler(OperationType opType) {
+        super(opType);
     }
-    return null;
-  }
+
+    @Override
+    protected Object run(Object info) throws IOException {
+        boolean retry = true;
+        boolean rollback = false;
+
+        int tryCount = 0;
+        IOException exception = null;
+
+        //try {
+        while (retry && tryCount < RETRY_COUNT) {
+            retry = true;
+            tryCount++;
+            exception = null;
+            rollback = false;
+            try {
+                NDC.push(opType.toString()); // Defines a context for every operation to track them in the logs easily.
+
+                //In a tx if the lock level is set to write, does 
+                //it mean that all the operations after seting the lock will use write lcok? 
+                //Light weight tx handler is sometimes called inside a bigger tx. 
+                //If in the outer tx lock level was set to some thing other than read-commited 
+                //then we will end up taking un necessary locks.
+                //To make sure that we done have this problem I explicitly set the locks to read-commited. 
+
+
+                //connector.beginTransaction();
+                //connector.readCommitted();
+                Object toReturn = performTask();
+                //connector.commit();
+                return toReturn;
+            } catch (PersistanceException ex) {
+                log.error("Could not perfortm task-" + ex);
+                rollback = true;
+                retry = true;
+            } catch (IOException ex) {
+                rollback = true;
+                exception = ex;
+            } catch (Exception ex) {
+                rollback = true;
+            } finally {
+                if (rollback && connector.isTransactionActive()) {
+                    log.error("HOP :: Perform rollback");
+                    connector.rollback();
+                }
+                NDC.pop();
+                if (tryCount == RETRY_COUNT && exception != null) {
+                    throw exception;
+                }
+
+
+            }
+        }
+        /*} finally {
+         if(connector.isTransactionActive()){
+         connector.rollback();
+         }
+         }*/
+        return null;
+    }
 }
