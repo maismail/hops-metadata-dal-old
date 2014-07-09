9dbfd7fb8247ee78dad572eef861094b46ba7485
nstanogias <niksta@kth.se>
2014-07-09 16:01:17 +0200

    Updated DelegationToken, DelegationKey

--- src/main/java/se/sics/hop/metadata/yarn/dal/DelegationKeyDataAccess.java
+++ src/main/java/se/sics/hop/metadata/yarn/dal/DelegationKeyDataAccess.java
@@ -7,8 +7,10 @@
 package se.sics.hop.metadata.yarn.dal;
 
 import java.util.Collection;
+import java.util.List;
 import se.sics.hop.exception.StorageException;
 import se.sics.hop.metadata.hdfs.dal.EntityDataAccess;
+import se.sics.hop.metadata.hdfs.entity.yarn.HopDelegationKey;
 
 /**
  *
@@ -18,4 +20,8 @@ public interface DelegationKeyDataAccess<T> extends EntityDataAccess {
     T findByKey(int key) throws StorageException;
 
     void prepare(Collection<T> modified, Collection<T> removed) throws StorageException;
+
+    public void createDTMasterKeyEntry(HopDelegationKey hopDelegationKey);
+
+    List<T> getAll() throws StorageException;
 }
--- src/main/java/se/sics/hop/metadata/yarn/dal/DelegationTokenDataAccess.java
+++ src/main/java/se/sics/hop/metadata/yarn/dal/DelegationTokenDataAccess.java
@@ -7,8 +7,10 @@
 package se.sics.hop.metadata.yarn.dal;
 
 import java.util.Collection;
+import java.util.List;
 import se.sics.hop.exception.StorageException;
 import se.sics.hop.metadata.hdfs.dal.EntityDataAccess;
+import se.sics.hop.metadata.hdfs.entity.yarn.HopDelegationToken;
 
 /**
  *
@@ -18,4 +20,8 @@ public interface DelegationTokenDataAccess<T> extends EntityDataAccess {
     T findBySeqNumber(int seqnumber) throws StorageException;
 
     void prepare(Collection<T> modified, Collection<T> removed) throws StorageException;
+
+    public void createDelegationTokenEntry(HopDelegationToken hopDelegationToken);
+    
+    List<T> getAll() throws StorageException;
 }
