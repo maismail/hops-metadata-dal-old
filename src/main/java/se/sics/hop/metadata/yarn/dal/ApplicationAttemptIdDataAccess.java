16fcc157a200c8f9a9182b4c78c895b0772f46d3
Nikos Stanogias <niksta@sics.se>
2014-08-26 14:14:49 +0200

    Fixed some bugs

--- src/main/java/se/sics/hop/metadata/yarn/dal/ApplicationAttemptIdDataAccess.java
+++ src/main/java/se/sics/hop/metadata/yarn/dal/ApplicationAttemptIdDataAccess.java
@@ -11,7 +11,7 @@ import se.sics.hop.metadata.hdfs.dal.EntityDataAccess;
  */
 public interface ApplicationAttemptIdDataAccess<T> extends EntityDataAccess {
 
-    T findById(String id) throws StorageException;
+    List<T> findByAttemptId(String id) throws StorageException;
 
     T findByAttemptIdAppId(int attemptId, int appId) throws StorageException; 
     
