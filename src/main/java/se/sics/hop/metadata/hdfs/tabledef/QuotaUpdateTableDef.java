1adce251c6b81aee04d6a0fdd9709d941bc7f659
Steffen Grohsschmiedt <git@grohsschmiedt.de>
2014-09-18 08:31:34 +0200

    Support for eventually consistent quota

---
+++ src/main/java/se/sics/hop/metadata/hdfs/tabledef/QuotaUpdateTableDef.java
@@ -0,0 +1,10 @@
+package se.sics.hop.metadata.hdfs.tabledef;
+
+public interface QuotaUpdateTableDef {
+
+  public static final String TABLE_NAME = "quota_update";
+  public static final String ID = "id";
+  public static final String INODE_ID = "inode_id";
+  public static final String NAMESPACE_DELTA = "namespace_delta";
+  public static final String DISKSPACE_DELTA = "diskspace_delta";
+}
