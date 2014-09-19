5f2d705a2753ab22bb8291f9000a1a083f23b010
gautier <gautier@sics.se>
2014-09-19 16:01:03 +0200

    updating the table corresponding to RMContainer

--- src/main/java/se/sics/hop/metadata/hdfs/entity/yarn/HopRMContainer.java
+++ src/main/java/se/sics/hop/metadata/hdfs/entity/yarn/HopRMContainer.java
@@ -11,20 +11,34 @@ public class HopRMContainer {
     private final String applicationAttemptIdID;
     private final String nodeIdID;
     private final String user;
-    private final String reservedNodeIdID;
-    private final int reservedPriorityID;
+//    private final String reservedNodeIdID;
+//    private final int reservedPriorityID;
     private final long starttime;
     private final long finishtime;
-
-    public HopRMContainer(String containerIdID, String applicationAttemptIdID, String nodeIdID, String user, String reservedNodeIdID, int reservedPriorityID, long starttime, long finishtime) {
+    private final String state;
+//    private final String reservedNodeHost;
+//    private final int reservedNodePort;
+    private final String finishedStatusState;
+    private final int exitStatus;
+    
+    public HopRMContainer(String containerIdID, String applicationAttemptIdID, 
+            String nodeIdID, String user, /*String reservedNodeIdID, 
+            int reservedPriorityID,*/ long starttime, long finishtime,
+            String state, /*String reservedNodeHost, Integer reservedNodePort,*/ 
+            String finishedStatusState, Integer exitStatus) {
         this.containerIdID = containerIdID;
         this.applicationAttemptIdID = applicationAttemptIdID;
         this.nodeIdID = nodeIdID;
         this.user = user;
-        this.reservedNodeIdID = reservedNodeIdID;
-        this.reservedPriorityID = reservedPriorityID;
+//        this.reservedNodeIdID = reservedNodeIdID;
+//        this.reservedPriorityID = reservedPriorityID;
         this.starttime = starttime;
         this.finishtime = finishtime;
+        this.state = state;
+//        this.reservedNodeHost = reservedNodeId;
+//        this.reservedNodePort = reservedNodePort;
+        this.finishedStatusState = finishedStatusState;
+        this.exitStatus = exitStatus;
     }
 
     public String getContainerIdID() {
@@ -43,13 +57,13 @@ public class HopRMContainer {
         return user;
     }
 
-    public String getReservedNodeIdID() {
-        return reservedNodeIdID;
-    }
+//    public String getReservedNodeIdID() {
+//        return reservedNodeIdID;
+//    }
 
-    public int getReservedPriorityID() {
-        return reservedPriorityID;
-    }
+//    public int getReservedPriorityID() {
+//        return reservedPriorityID;
+//    }
 
     public long getStarttime() {
         return starttime;
@@ -58,4 +72,25 @@ public class HopRMContainer {
     public long getFinishtime() {
         return finishtime;
     }
+
+  public String getState() {
+    return state;
+  }
+
+//  public String getReservedNodeId() {
+//    return reservedNodeHost;
+//  }
+//
+//  public int getReservedNodePort() {
+//    return reservedNodePort;
+//  }
+
+  public String getFinishedStatusState() {
+    return finishedStatusState;
+  }
+
+  public int getExitStatus() {
+    return exitStatus;
+  }
+    
 }
