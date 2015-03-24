d13f57421aefebb71036e78df045deb7a97f7cc4
Steffen Grohsschmiedt <steffeng@sics.se>
2015-03-24 10:22:19 +0100

    Remove duplicated erasure coding classes

--- src/main/java/io/hops/metadata/hdfs/entity/EncodingStatus.java
+++ src/main/java/io/hops/metadata/hdfs/entity/EncodingStatus.java
@@ -1,32 +1,69 @@
 package io.hops.metadata.hdfs.entity;
 
+import io.hops.metadata.common.CounterType;
+import io.hops.metadata.common.FinderType;
+
 public class EncodingStatus {
 
-  /* Source file status */
-  public static final int ENCODING_REQUESTED = 0;
-  public static final int ENCODING_ACTIVE = 1;
-  public static final int ENCODED = 2;
-  public static final int REPAIR_ACTIVE = 3;
-  public static final int REPAIR_FAILED = 4;
-  public static final int ENCODING_FAILED = 5;
-  public static final int ENCODING_CANCELED = 6;
-  public static final int REPAIR_CANCELED = 7;
-  public static final int REPAIR_REQUESTED = 8;
-  public static final int DELETED = 9;
+  public static enum Status {
+    NOT_ENCODED,
+    ENCODING_REQUESTED,
+    ENCODING_ACTIVE,
+    ENCODING_CANCELED,
+    ENCODING_FAILED,
+    ENCODED,
+    REPAIR_REQUESTED,
+    REPAIR_ACTIVE,
+    REPAIR_CANCELED,
+    REPAIR_FAILED,
+    DELETED,
+  }
 
-  /* Parity file status */
-  public static final int PARITY_HEALTHY = 0;
-  public static final int PARITY_REPAIR_REQUESTED = 1;
-  public static final int PARITY_REPAIR_ACTIVE = 2;
-  public static final int PARITY_REPAIR_FAILED = 3;
-  public static final int PARITY_REPAIR_CANCELED = 4;
+  public static enum ParityStatus {
+    HEALTHY,
+    REPAIR_REQUESTED,
+    REPAIR_ACTIVE,
+    REPAIR_CANCELED,
+    REPAIR_FAILED
+  }
+
+  public static enum Counter implements CounterType<EncodingStatus> {
+    RequestedEncodings,
+    ActiveEncodings,
+    ActiveRepairs,
+    Encoded;
+
+    @Override
+    public Class getType() {
+      return EncodingStatus.class;
+    }
+  }
+
+  public static enum Finder implements FinderType<EncodingStatus> {
+    ByInodeId,
+    ByParityInodeId;
+
+    @Override
+    public Class getType() {
+      return EncodingStatus.class;
+    }
+
+    @Override
+    public Annotation getAnnotated() {
+      switch (this){
+        case ByInodeId: return Annotation.PrimaryKey;
+        case ByParityInodeId: return Annotation.IndexScan;
+        default: throw new IllegalStateException();
+      }
+    }
+
+  }
 
   private Integer inodeId;
   private Integer parityInodeId;
-  private Integer status;
-  private Integer parityStatus;
-  private String codec;
-  private Short targetReplication;
+  private Status status;
+  private ParityStatus parityStatus;
+  private EncodingPolicy encodingPolicy;
   private Long statusModificationTime;
   private Long parityStatusModificationTime;
   private String parityFileName;
@@ -38,18 +75,32 @@ public class EncodingStatus {
 
   }
 
-  public EncodingStatus(Integer inodeId, Integer parityInodeId, Integer status,
-      String codec, Short targetReplication,
-      Long statusModificationTime, Integer parityStatus,
-      Long parityStatusModificationTime, String parityFileName,
-      Integer lostBlocks, Integer lostParityBlocks, Boolean revoked) {
+  public EncodingStatus(Status status) {
+    this.status = status;
+  }
+
+  public EncodingStatus(Status status, EncodingPolicy encodingPolicy, String parityFileName) {
+    this.status = status;
+    this.encodingPolicy = encodingPolicy;
+    this.parityFileName = parityFileName;
+  }
+
+  public EncodingStatus(Integer inodeId, Status status, EncodingPolicy encodingPolicy, Long statusModificationTime) {
+    this.inodeId = inodeId;
+    this.status = status;
+    this.encodingPolicy = encodingPolicy;
+    this.statusModificationTime = statusModificationTime;
+  }
+
+  public EncodingStatus(Integer inodeId, Integer parityInodeId, Status status, ParityStatus parityStatus,
+      EncodingPolicy encodingPolicy, Long statusModificationTime, Long parityStatusModificationTime,
+      String parityFileName, Integer lostBlocks, Integer lostParityBlocks, Boolean revoked) {
     this.inodeId = inodeId;
     this.parityInodeId = parityInodeId;
     this.status = status;
-    this.codec = codec;
-    this.targetReplication = targetReplication;
-    this.statusModificationTime = statusModificationTime;
     this.parityStatus = parityStatus;
+    this.encodingPolicy = encodingPolicy;
+    this.statusModificationTime = statusModificationTime;
     this.parityStatusModificationTime = parityStatusModificationTime;
     this.parityFileName = parityFileName;
     this.lostBlocks = lostBlocks;
@@ -73,36 +124,28 @@ public class EncodingStatus {
     this.parityInodeId = parityInodeId;
   }
 
-  public Integer getStatus() {
+  public Status getStatus() {
     return status;
   }
 
-  public void setStatus(Integer status) {
+  public void setStatus(Status status) {
     this.status = status;
   }
 
-  public Integer getParityStatus() {
+  public ParityStatus getParityStatus() {
     return parityStatus;
   }
 
-  public void setParityStatus(Integer parityStatus) {
+  public void setParityStatus(ParityStatus parityStatus) {
     this.parityStatus = parityStatus;
   }
 
-  public String getCodec() {
-    return codec;
+  public EncodingPolicy getEncodingPolicy() {
+    return encodingPolicy;
   }
 
-  public void setCodec(String codec) {
-    this.codec = codec;
-  }
-
-  public Short getTargetReplication() {
-    return targetReplication;
-  }
-
-  public void setTargetReplication(Short targetReplication) {
-    this.targetReplication = targetReplication;
+  public void setEncodingPolicy(EncodingPolicy encodingPolicy) {
+    this.encodingPolicy = encodingPolicy;
   }
 
   public Long getStatusModificationTime() {
@@ -153,15 +196,60 @@ public class EncodingStatus {
     this.revoked = revoked;
   }
 
+  public boolean isEncoded() {
+    switch (status) {
+      case ENCODED:
+      case REPAIR_REQUESTED:
+      case REPAIR_ACTIVE:
+      case REPAIR_CANCELED:
+      case REPAIR_FAILED:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  public boolean isCorrupt() {
+    switch (status) {
+      case REPAIR_REQUESTED:
+      case REPAIR_ACTIVE:
+      case REPAIR_CANCELED:
+      case REPAIR_FAILED:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  public boolean isParityRepairActive() {
+    switch (parityStatus) {
+      case REPAIR_ACTIVE:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  public boolean isParityCorrupt() {
+    switch (parityStatus) {
+      case REPAIR_ACTIVE:
+      case REPAIR_REQUESTED:
+      case REPAIR_CANCELED:
+      case REPAIR_FAILED:
+        return true;
+      default:
+        return false;
+    }
+  }
+
   @Override
   public String toString() {
-    return "HopEncodingStatus{" +
+    return "EncodingStatus{" +
         "inodeId=" + inodeId +
         ", parityInodeId=" + parityInodeId +
         ", status=" + status +
         ", parityStatus=" + parityStatus +
-        ", codec='" + codec + '\'' +
-        ", targetReplication=" + targetReplication +
+        ", encodingPolicy=" + encodingPolicy +
         ", statusModificationTime=" + statusModificationTime +
         ", parityStatusModificationTime=" + parityStatusModificationTime +
         ", parityFileName='" + parityFileName + '\'' +
@@ -169,5 +257,52 @@ public class EncodingStatus {
         ", lostParityBlocks=" + lostParityBlocks +
         ", revoked=" + revoked +
         '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    EncodingStatus status1 = (EncodingStatus) o;
+
+    if (encodingPolicy != null ? !encodingPolicy.equals(status1.encodingPolicy) : status1.encodingPolicy != null)
+      return false;
+    if (inodeId != null ? !inodeId.equals(status1.inodeId) : status1.inodeId != null) return false;
+    if (lostBlocks != null ? !lostBlocks.equals(status1.lostBlocks) : status1.lostBlocks != null) return false;
+    if (lostParityBlocks != null ? !lostParityBlocks.equals(
+        status1.lostParityBlocks) : status1.lostParityBlocks != null)
+      return false;
+    if (parityFileName != null ? !parityFileName.equals(status1.parityFileName) : status1.parityFileName != null)
+      return false;
+    if (parityInodeId != null ? !parityInodeId.equals(status1.parityInodeId) : status1.parityInodeId != null)
+      return false;
+    if (parityStatus != status1.parityStatus) return false;
+    if (parityStatusModificationTime != null ? !parityStatusModificationTime.equals(
+        status1.parityStatusModificationTime) : status1.parityStatusModificationTime != null)
+      return false;
+    if (revoked != null ? !revoked.equals(status1.revoked) : status1.revoked != null) return false;
+    if (status != status1.status) return false;
+    if (statusModificationTime != null ? !statusModificationTime.equals(
+        status1.statusModificationTime) : status1.statusModificationTime != null)
+      return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = inodeId != null ? inodeId.hashCode() : 0;
+    result = 31 * result + (parityInodeId != null ? parityInodeId.hashCode() : 0);
+    result = 31 * result + (status != null ? status.hashCode() : 0);
+    result = 31 * result + (parityStatus != null ? parityStatus.hashCode() : 0);
+    result = 31 * result + (encodingPolicy != null ? encodingPolicy.hashCode() : 0);
+    result = 31 * result + (statusModificationTime != null ? statusModificationTime.hashCode() : 0);
+    result = 31 * result + (parityStatusModificationTime != null ? parityStatusModificationTime.hashCode() : 0);
+    result = 31 * result + (parityFileName != null ? parityFileName.hashCode() : 0);
+    result = 31 * result + (lostBlocks != null ? lostBlocks.hashCode() : 0);
+    result = 31 * result + (lostParityBlocks != null ? lostParityBlocks.hashCode() : 0);
+    result = 31 * result + (revoked != null ? revoked.hashCode() : 0);
+    return result;
   }
 }
